f58b9ec78d121f3571e745ab28f02f6e093700a1
Usman Asif <82535372+UsmanAsif193@users.noreply.github.com>
2022-04-19 02:10:38 +0500

    feat: design of todo app completed

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import SingleTask from "./components/SingleTask";
 
 export default function App() {
@@ -14,6 +22,22 @@ export default function App() {
           <SingleTask />
         </View>
       </View>
+      {/* Write a Task */}
+      <KeyboardAvoidingView
+        style={styles.writeTaskWrapper}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder="Write a Task"
+          placeholderTextColor="#898989"
+        />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -33,5 +57,40 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "white",
+    borderRadius: 60,
+    textAlign: "center",
+    width: 250,
+    color: "#2a2a2a",
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+  },
+  addText: {
+    color: "#898989",
+    fontSize: 40,
+    fontStyle: "normal",
+    fontWeight: "200",
+    fontFamily: "Arial",
+    textAlign: "center",
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
   },
 });
--- components/SingleTask/index.js
+++ components/SingleTask/index.js
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginBottom: 20,
+    boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
   },
   itemLeft: {
     flexDirection: "row",
